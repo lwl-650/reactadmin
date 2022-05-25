bee74ff8888028d8997376e543a05a36f0d52cf6
无敌暴龙战士 <75242799+lwl-650@users.noreply.github.com>
2022-05-25 15:36:20 +0800

    fix: index中使用事件代理绑定onClick

--- src/view/index/index.tsx
+++ src/view/index/index.tsx
@@ -9,7 +9,6 @@ import {
 import "./index.scss";
 import { findId, findAll, delById, addUser } from "../../http/api";
 import {
-
   Input
 } from "antd";
 
@@ -21,15 +20,24 @@ interface IndexProps {
 
 
 const Index: FC<IndexProps> = ({ onClick, children }): ReactElement => {
-  
+
+
+  const [list, setList] = useState([1, 2, 34, 5, 6])
 
   useEffect(() => {
   }, []);
 
+  function fn(e:any):void{
+     console.log(e)
+    console.log(e.target.getAttribute('data-id'))
+  }
 
   return (
-    <div className="all">
-     index
+    <div className="all" onClick={fn}>
+      index
+      {
+        list.map(item => <button key={item} data-id={item}>{item}</button>)
+      }
     </div>
   )
 };
